0ca11d4e29da9c96cc72c3ad853d5bee8551ed79
UnyCat7 <152312900+UnyCat7@users.noreply.github.com>
2025-09-05 13:16:22 +0300

    Fixes
    fixes

--- Optical_Simulation.js
+++ Optical_Simulation.js
@@ -27,7 +27,7 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: C:\Users\papag\AppData\Local\Temp\tmpr95un2yl.js
+// include: C:\Users\papag\AppData\Local\Temp\tmp_8rmu1yl.js
 
   Module['expectedDataFileDownloads'] ??= 0;
   Module['expectedDataFileDownloads']++;
@@ -215,21 +215,21 @@ Module['FS_createPath']("/resources", "textures", true, true);
 
   })();
 
-// end include: C:\Users\papag\AppData\Local\Temp\tmpr95un2yl.js
-// include: C:\Users\papag\AppData\Local\Temp\tmpx887h_xo.js
+// end include: C:\Users\papag\AppData\Local\Temp\tmp_8rmu1yl.js
+// include: C:\Users\papag\AppData\Local\Temp\tmpbihrnqqh.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if ((typeof ENVIRONMENT_IS_WASM_WORKER != 'undefined' && ENVIRONMENT_IS_WASM_WORKER) || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD) || (typeof ENVIRONMENT_IS_AUDIO_WORKLET != 'undefined' && ENVIRONMENT_IS_AUDIO_WORKLET)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: C:\Users\papag\AppData\Local\Temp\tmpx887h_xo.js
-// include: C:\Users\papag\AppData\Local\Temp\tmpe3m4g1v8.js
+  // end include: C:\Users\papag\AppData\Local\Temp\tmpbihrnqqh.js
+// include: C:\Users\papag\AppData\Local\Temp\tmps53k9h0u.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: C:\Users\papag\AppData\Local\Temp\tmpe3m4g1v8.js
+  // end include: C:\Users\papag\AppData\Local\Temp\tmps53k9h0u.js
 
 
 var arguments_ = [];
