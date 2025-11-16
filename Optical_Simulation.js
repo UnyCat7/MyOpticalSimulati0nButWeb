3a941a6e0caf4d323a7e76b694b7286a87b9b430
UnyCat7 <152312900+UnyCat7@users.noreply.github.com>
2025-11-16 20:02:40 +0300

    Add files via upload
    This is now a real light sim... The light seems to be still broken.

--- Optical_Simulation.js
+++ Optical_Simulation.js
@@ -27,7 +27,7 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: C:\Users\papag\AppData\Local\Temp\tmpn8rjlh6v.js
+// include: C:\Users\Heinrich\AppData\Local\Temp\tmpr_xl7u4h.js
 
   Module['expectedDataFileDownloads'] ??= 0;
   Module['expectedDataFileDownloads']++;
@@ -218,21 +218,21 @@ Module['FS_createPath']("/resources", "textures", true, true);
 
   })();
 
-// end include: C:\Users\papag\AppData\Local\Temp\tmpn8rjlh6v.js
-// include: C:\Users\papag\AppData\Local\Temp\tmpe7tc5lai.js
+// end include: C:\Users\Heinrich\AppData\Local\Temp\tmpr_xl7u4h.js
+// include: C:\Users\Heinrich\AppData\Local\Temp\tmp4feavmw8.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if ((typeof ENVIRONMENT_IS_WASM_WORKER != 'undefined' && ENVIRONMENT_IS_WASM_WORKER) || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD) || (typeof ENVIRONMENT_IS_AUDIO_WORKLET != 'undefined' && ENVIRONMENT_IS_AUDIO_WORKLET)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: C:\Users\papag\AppData\Local\Temp\tmpe7tc5lai.js
-// include: C:\Users\papag\AppData\Local\Temp\tmpd1rhux_j.js
+  // end include: C:\Users\Heinrich\AppData\Local\Temp\tmp4feavmw8.js
+// include: C:\Users\Heinrich\AppData\Local\Temp\tmp8f__2e_e.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: C:\Users\papag\AppData\Local\Temp\tmpd1rhux_j.js
+  // end include: C:\Users\Heinrich\AppData\Local\Temp\tmp8f__2e_e.js
 
 
 var arguments_ = [];
